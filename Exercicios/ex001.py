e57999e536bf7fdb998a16d30815b90a2b7e5be0
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2025-03-13 13:09:42 -0300

    first class

---
+++ Exercicios/ex001.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+n1 = int(input("informe um valor: "))
+n2 = int(input("informe outro valor: "))
+s = n1 + n2
+print("A soma entre {} e {} vale {}".format(n1, n2, s))
