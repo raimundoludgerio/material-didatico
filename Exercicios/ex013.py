8000c354a61daff68a605edbe5fdb7e3a828c217
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2025-03-25 08:38:36 -0300

    refactor: otimized imports

--- Exercicios/ex013.py
+++ Exercicios/ex013.py
@@ -1,10 +1,10 @@
 # ex013: sorteio de uma lista
 # Solução feita por: José Augusto Batista do Nascimento
-import random
+from random import choice
 
 alunos = ['Cleitin', 'Jubileu', 'Jeremias','Zé']
 
-print(random.choice(alunos))
+print(choice(alunos))
 
 
 # Solução 02: Feita pelo professor
@@ -15,7 +15,7 @@ aluno4 = input('Qual o nome do quarto aluno? ')
 
 alunos = [aluno1, aluno2, aluno3, aluno4]
 
-aluno_escolhido = random.choice(alunos)
+aluno_escolhido = choice(alunos)
 titulo = "O aluno escolhido foi:"
 print("=" * 80)
 print(f"{titulo:^80}")
