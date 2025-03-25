9f18d64742870013e1d86daf3b0d70fd41f73d96
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2025-03-25 08:34:01 -0300

    feat: implemented new examplo using arrays

--- Exercicios/ex013.py
+++ Exercicios/ex013.py
@@ -9,9 +9,9 @@ print(random.choice(alunos))
 
 # Solução 02: Feita pelo professor
 aluno1 = input('Qual o nome do primeiro aluno? ')
-aluno2 = input('Qual o nome do primeiro aluno? ')
-aluno3 = input('Qual o nome do primeiro aluno? ')
-aluno4 = input('Qual o nome do primeiro aluno? ')
+aluno2 = input('Qual o nome do segundo aluno? ')
+aluno3 = input('Qual o nome do terceiro aluno? ')
+aluno4 = input('Qual o nome do quarto aluno? ')
 
 alunos = [aluno1, aluno2, aluno3, aluno4]
 
---
+++ Exercicios/ex014.py
@@ -0,0 +1,29 @@
+# ex014: sorteio de ordem de alunos
+# Solução feita por: Ana Alice Lopes Alves
+import random
+
+alunos = input("Digite o nome dos alunos: ").split(",")
+
+l_alunos = [aluno.strip() for aluno in alunos]
+
+random.shuffle(l_alunos)
+
+print(l_alunos)
+
+# Solução alternativa desenvolvida pelo professor
+
+aluno1 = input('Qual o nome do primeiro aluno? ')
+aluno2 = input('Qual o nome do segundo aluno? ')
+aluno3 = input('Qual o nome do tereceiro aluno? ')
+aluno4 = input('Qual o nome do quarto aluno? ')
+
+alunos = [aluno1, aluno2, aluno3, aluno4]
+random.shuffle(alunos)
+titulo = "A ordem para apagar o quadro é:"
+print("=" * 40)
+print(f"{titulo:^40}")
+print(f"1º {alunos[0]:}")
+print(f"2º {alunos[1]:}")
+print(f"3º {alunos[2]:}")
+print(f"4º {alunos[3]:}")
+print("=" * 40)
