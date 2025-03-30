e6db390712279750e34e765b7f49eac02d502414
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2025-03-30 12:51:07 -0300

    refactor: update files structures

---
+++ Exercicios/desafios-b/aula.py
@@ -0,0 +1,11 @@
+# print('QUESTÃO 4')
+
+# num_qualq = int(input('Digite um número qualquer: '))
+
+# for i in range(num_qualq,num_qualq*11,num_qualq):
+#     print(i)
+
+preço = float(input('digite o valor do produto: '))
+desconto = (5/preço) * 100
+com_desconto = preço - desconto
+print(f'o produto com desconto e {com_desconto}')
