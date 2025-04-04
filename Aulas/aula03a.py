d2e844a271af32f517f4c424e97dcf89330be2dd
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2025-04-04 10:45:39 -0300

    ed-feat: imlemented new examples

--- Aulas/aula03a.py
+++ Aulas/aula03a.py
@@ -1,10 +1,12 @@
 from random import randint
-while True:
-    numero_maquina = randint(0, 10)
+opcao = True
+while opcao:
+    numero_maquina = randint(0, 5)
     numero_usuario = int(input("Informe um número entre 0 e 10: "))
     print(f"Máquina: {numero_maquina}")
     print(f"Usuário: {numero_usuario}")
     if numero_usuario == numero_maquina:
         print("Você acertou!")
+        opcao = False
     else:
         print("Que pena, tente novamente")
