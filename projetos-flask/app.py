0aa96be3670ed9645c35f6b74a6938c7517b1985
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2025-05-15 22:28:15 -0300

    refactor: updated route to render a template html

--- projetos-flask/app.py
+++ projetos-flask/app.py
@@ -1,10 +1,13 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def incial():
     return 'Olá Mundo!'
+
+@app.route('/add')
+def adicionar():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
