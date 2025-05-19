bcd5a05959ccc6707d675221acbf664473f17079
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2025-05-19 20:56:54 -0300

    feat:implemented a new example

---
+++ Aulas/funcoes/aula01.py
@@ -0,0 +1,8 @@
+def calcular_area_retangulo(largura, altura):
+    area = largura * altura
+    return area
+
+# Agora podemos reutilizar a função várias vezes
+print("Área 1:", calcular_area_retangulo(5, 10))
+print("Área 2:", calcular_area_retangulo(3, 7))
+print("Área 3:", calcular_area_retangulo(8, 4))
