933954f878d6b8722d7e2ab6fbc01d97528462e4
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2025-09-02 09:22:20 -0300

    test: implemented a new class example

--- Aulas/estrutura de dados/aula01/aula-introducao.py
+++ Aulas/estrutura de dados/aula01/aula-introducao.py
@@ -6,18 +6,31 @@ sobrenomes = ['Silva', 'Souza', 'Oliveira', 'Santos', 'Lima', 'Costa', 'Ferreira
 
 estudantes = []
 
-for i in range(10):
+for i in range(50):
     estudante = {
         "nome": random.choice(nomes),
         "sobrenome": random.choice(sobrenomes),
-        "idade": random.randint(14, 25)
+        "idade": random.randint(14, 25),
+        "matricula": random.randint(1000,1500000)
     }
     estudantes.append(estudante)
 
 # Função para retorna um estudante
 
+def escolha_estudante(lista_estudante, matricula):
+    encontrado = "Não encontrado"
+    for aluno in lista_estudante:
+        if aluno["matricula"] == matricula:
+            print("Encontreiiii")
+            encontrado = aluno
+    return encontrado
+
+matricula = estudantes[25]["matricula"]
+
+estudante_encontrado = escolha_estudante(estudantes, 8763387)
+print(estudante_encontrado)
 # Reorganização dos dados
 
 # nova função com busca mais otimizada
-
-print(estudantes)
+#for estudante in estudantes:
+#    print(estudante["nome"], estudante["sobrenome"], estudante["matricula"])
---
+++ Aulas/tuplas/exemplos/ex001.py
@@ -0,0 +1,4 @@
+dias_semana = ["Segunda", "Terça", "Quarta"]
+dias_semana[1] = "Sexta"
+print(dias_semana)
+
