6ccd2dd491ff3f9eeb616e200e50117ed5685cd2
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2025-10-07 20:26:28 -0300

    feat: add a new example flask

--- projetos-flask/projeto_wtf/app/users/forms.py
+++ projetos-flask/projeto_wtf/app/users/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField, BooleanField
-from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError, Optional, regexp
 import email_validator
 
 class LoginForm(FlaskForm):
@@ -8,6 +8,11 @@ class LoginForm(FlaskForm):
     password = PasswordField('Senha', validators=[DataRequired()])
     remember_me = BooleanField('Lembrar de mim')
     submit = SubmitField('Entrar')
+
+class Login2(FlaskForm):
+    username = StringField("Usuário", validators=[DataRequired(), Length(min=5)])
+    senha = PasswordField("Senha", validators=[DataRequired()])
+    botao = SubmitField("Logar")
 
 class RegistrationForm(FlaskForm):
     username = StringField('Nome de Usuário', 
@@ -22,11 +27,37 @@ class RegistrationForm(FlaskForm):
     
     # Validação personalizada
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('Este nome de usuário já está em uso.')
+       pass
     
     def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError('Este email já está cadastrado.')
+        pass
+        
+class ContactForm(FlaskForm):
+    name = StringField('Nome', validators=[
+        DataRequired(message='O nome é obrigatório'),
+        Length(min=2, max=50, message='O nome deve ter entre 2 e 50 caracteres')
+    ])
+    
+    email = StringField('Email', validators=[
+        DataRequired(),
+        Email(message='Digite um email válido')
+    ])
+    
+    phone = StringField('Telefone', validators=[
+        Optional(),
+        regexp(r'^\(\d{2}\)\d{4,5}-\d{4}$', message='Formato: (11)99999-9999')
+    ])
+    
+    message = TextAreaField('Mensagem', validators=[
+        DataRequired(),
+        Length(min=10, max=500, message='A mensagem deve ter entre 10 e 500 caracteres')
+    ])
+    
+    category = SelectField('Categoria', choices=[
+        ('duvida', 'Dúvida'),
+        ('sugestao', 'Sugestão'),
+        ('reclamacao', 'Reclamação'),
+        ('elogio', 'Elogio')
+    ], validators=[DataRequired()])
+    
+    submit = SubmitField('Enviar Mensagem')
--- projetos-flask/projeto_wtf/app/users/routes.py
+++ projetos-flask/projeto_wtf/app/users/routes.py
@@ -1,6 +1,6 @@
 from flask import flash, render_template, redirect, url_for
 from . import bp
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm, Login2
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
@@ -10,3 +10,36 @@ def register():
         # Redireciona para a mesma página ou para uma página de sucesso
         return redirect(url_for('register'))  
     return render_template('cadastro.html', title='Registrar', form=form)
+
+@bp.route('/', methods=['GET'])
+def index():
+    return "página index"
+@bp.route('/login', methods=['GET'])
+def login():
+    form = LoginForm()
+    
+    if form.validate_on_submit():
+        # Simulação de autenticação
+        email = form.email.data
+        password = form.password.data
+        
+        # Em uma aplicação real, verifique no banco de dados
+        flash('Login realizado com sucesso!', 'success')
+        return redirect(url_for('index'))
+       
+    return render_template('login.html', form=form)
+
+
+@bp.route('/login2', methods=['GET', 'POST'])
+def login2():
+    formulario = Login2()
+    if formulario.validate_on_submit():
+        flash("Login realizado com sucesso")
+        usuario = formulario.username.data
+        password = formulario.senha.data
+        usuario = {
+            "user": usuario,
+            "password": password
+        }
+        return render_template("home.html", user=usuario)
+    return render_template("login2.html", formulario=formulario)
