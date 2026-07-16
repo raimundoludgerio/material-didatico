3859f81c85eb939c5cf6a65465e37ee244d98a92
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2026-07-16 10:19:26 -0300

    refactor: updated a new example

--- projetos-flask/projeto-heranca/app.py
+++ projetos-flask/projeto-heranca/app.py
@@ -1,2 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/produtos")
+def produtos():
+    return render_template("produtos.html")
+
+
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
+
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
